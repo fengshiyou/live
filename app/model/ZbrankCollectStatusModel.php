2a3cd3175424a30f13323be7d36c968fba6fd795
fengshiyou <501453944@qq.com>
2017-11-09 14:12:54 +0800

    test db connect

---
+++ app/model/ZbrankCollectStatusModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ZbrankCollectStatusModel extends Model
+{
+    //
+    protected $table = "zbrank_collect_status";
+}
