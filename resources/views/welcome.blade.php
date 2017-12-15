bd6e361c817377401aac6b5187ebf33f39c7a16f
fengshiyou <501453944@qq.com>
2017-12-15 15:22:19 +0800

    新版

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,31 +65,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+    <a href="tel:10086">10086</a>
+    <a href="wtai://wp/mc;10086">10086</a>
     </body>
 </html>
