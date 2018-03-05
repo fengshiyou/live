0d41610e24c34ad448cdc7b1f931cf3cd0a050d6
fengshiyou <501453944@qq.com>
2018-03-05 22:51:30 +0800

    新版

---
+++ app/Http/Controllers/ImgAdsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\model\liverAddrModel;
+use App\model\ZbrankCollectStatusModel;
+use App\model\zbrankPlatModel;
+use App\model\zbrankRank;
+use App\model\zbrankRankModel;
+use Illuminate\Http\Request;
+use DB;
+
+class RankController extends Controller
+{
+    public function index()
+    {
+        return view('rank');
+    }
+
+    public function getImgAds()
+    {
+        $data = array(
+            'a'=>'a'
+        );
+        resp_suc($data);
+    }
+
+}
